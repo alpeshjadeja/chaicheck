002dc282ed326e9e88f83bf4608c61d9686cfa01
alpeshjadeja <alpeshjadeja@gmail.com>
2025-11-26 23:42:26 -0500

    Local changes pushed to github

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,10 @@ plugins {
 android {
     namespace = "com.chaicheck.chaicheck"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -28,6 +29,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
